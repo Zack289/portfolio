932f4ef28c3bc86a29448f08ca68d0c688ee925a
Zack289 <ozack911@gmail.com>
2026-02-01 22:13:59 +0545

    Added some fewatures for the crusor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/home/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import CursorBg from './components/CrusorBg'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/home/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import CursorBg from "./components/CrusorBg";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div>
       <CursorBg />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+        }}
+      />
       <Navbar />
       <Hero />
       <About />
@@ -18,7 +33,7 @@ function App() {
       <Skills />
       <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
